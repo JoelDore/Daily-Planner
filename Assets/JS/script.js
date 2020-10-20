8605140e7d628478b13dcd49414e8a15dd1177a2
JES20 <dore.joel.dore@gmail.com>
2020-10-20 14:34:51 -0400

    Updated index.html with hour text, linked pseudocoded js

---
+++ Assets/JS/script.js
@@ -0,0 +1,20 @@
+// Get Current Date
+// Assign to variable
+
+// Update #currentDay textContent as 'dddd, MMMM Do'
+
+// * Assign HTML ids to buttons, textareas *
+// // (indicate timeblock in 24-hr)
+
+// .saveBtn event listener
+// // update local storage with stringified {btn id, textarea(id).text}
+
+// If #id (24-hr) isBefore/isSame/isAfter current HH  
+// Then update color of textarea to .past/.present/.future
+
+// If current HH:mm:ss isSame 00:00:00
+// Then reset localstorage & textareas
+// (check Moment documentation for date change methods)
+
+
+/* handle editing text, NOT saving, exiting textarea? */
